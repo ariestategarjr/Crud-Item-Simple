02aeb22ee4b28ca7a83b8faa45b6f3d3c2ff2f26
AriestaTegar-JR <ariestategarjr87@gmail.com>
2022-12-20 15:57:35 +0700

    update index.js make route get for homepage

--- index.js
+++ index.js
@@ -3,10 +3,25 @@ const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
 const db = require('./connection');
+const response = require('./response');
 
+// connect to database
+db.connect((err) => {
+    if(err) throw err;
+    console.log("MySQL connected...");
+});
+
+// use bodyparser
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
+// route for homepage
+app.get("/", (req, res) => {
+    const data = response(200, "data", "OK", res);
+});
+
+// test server
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}...`);
 });
 
