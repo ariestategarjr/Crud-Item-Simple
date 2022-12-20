688211059dd666cc262a53017d2c1d5198a1d8e9
AriestaTegar-JR <ariestategarjr87@gmail.com>
2022-12-20 19:12:36 +0700

    update index.js make get query and render

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = 3000;
 const bodyParser = require('body-parser');
 const db = require('./connection');
 const response = require('./response');
+const { get } = require('http');
 
 // connect to database
 db.connect((err) => {
@@ -13,7 +14,7 @@ db.connect((err) => {
 });
 
 // set views engine
-app.set("views", path.join(__dirname, 'views'));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 // use bodyparser
@@ -23,7 +24,12 @@ app.use('/assets', express.static(__dirname + '/public'));
 
 // route for homepage
 app.get("/", (req, res) => {
-    const data = response(200, "data", "OK", res);
+    // const data = response(200, "data", "OK", res);
+    const getSQL = "SELECT * FROM stock";
+    db.query(getSQL, (err, result) => {
+        const items = JSON.parse(JSON.stringify(result));
+        res.render("view_stock", { items: items, title: "Simple Stock Items App" });
+    });
 });
 
 // test server
