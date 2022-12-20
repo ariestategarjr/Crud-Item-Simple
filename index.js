2ea049956647da632e67fb5eb78dff306e57a5c1
AriestaTegar-JR <ariestategarjr87@gmail.com>
2022-12-20 17:25:13 +0700

    update index.js set views to view_stock.ejs and use static folder to public

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -11,9 +12,14 @@ db.connect((err) => {
     console.log("MySQL connected...");
 });
 
+// set views engine
+app.set("views", path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+
 // use bodyparser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/assets', express.static(__dirname + '/public'));
 
 // route for homepage
 app.get("/", (req, res) => {
